00bc2177a05789780108bb3d2dec7978521b06b0
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2026-05-16 14:30:08 +0530

    Complete chapter 20, project 1

---
+++ ch20-project1/Exercise.java
@@ -0,0 +1,53 @@
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+        PriorityQueue<Integer> queue2 = new PriorityQueue<>();
+
+        System.out.print("Enter integers for priority queue 1: ");
+        String inputLine1 = input.nextLine();
+
+        System.out.print("Enter integers for priority queue 2: ");
+        String inputLine2 = input.nextLine();
+
+        input.close();
+
+        populateQueue(queue1, inputLine1);
+        populateQueue(queue2, inputLine2);
+
+        System.out.println("The union of the two priority queues is");
+        PriorityQueue<Integer> union = new PriorityQueue<>(queue1);
+        union.addAll(queue2);
+        printAndEmptyQueue(union);
+
+        System.out.println("The difference of the two priority queues is");
+        PriorityQueue<Integer> difference = new PriorityQueue<>(queue1);
+        difference.removeAll(queue2);
+        printAndEmptyQueue(difference);
+
+        System.out.println("The intersection of the two priority queues is");
+        PriorityQueue<Integer> intersection = new PriorityQueue<>(queue1);
+        intersection.retainAll(queue2);
+        printAndEmptyQueue(intersection);
+    }
+
+    private static void populateQueue(PriorityQueue<Integer> queue, String inputLine) {
+        Scanner lineScanner = new Scanner(inputLine);
+
+        while (lineScanner.hasNextInt()) {
+            queue.offer(lineScanner.nextInt());
+        }
+    }
+
+    private static void printAndEmptyQueue(PriorityQueue<Integer> queue) {
+        while (!queue.isEmpty()) {
+            System.out.print(queue.remove() + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ ch20-project1/README.md
@@ -0,0 +1,16 @@
+# Perform set operations on priority queues
+
+Write a program that creates two priority queues for integers. Prompt the user to enter two lines of integers.
+
+Read each line of integers as a string and extract the integers from the string and add the integers to the two queues, respectively. Display their union, difference, and intersection. Note that the priority queue can have duplicates. The union of two priority queues may have duplicates. Display all output in increasing order.
+
+## Sample Run
+
+> Enter integers for priority queue 1: 1 9 2 30 21 4 1\
+> Enter integers for priority queue 2: 12 19 2 10 12 1\
+> The union of the two priority queues is\
+> 1 1 1 2 2 4 9 10 12 12 19 21 30\
+> The difference of the two priority queues is\
+> 4 9 21 30\
+> The intersection of the two priority queues is\
+> 1 1 2
